9fac8053072ef8acaba7586da0a80e671d31f347
Himanshu Koshti <himfeedback74@gmail.com>
2024-10-14 22:34:39 +0530

    Initial commit

---
+++ src/main/java/com/GatewayApplication/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.GatewayApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
